677694ac2e5270d3308188c5d6153e51390eff0b
Marko Cupic <marko.cupic@tron-it.com>
2022-02-14 12:25:06 +0100

    Update home.page.ts

--- ionic-lincare-ordering-app/src/app/home/home.page.ts
+++ ionic-lincare-ordering-app/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
+
+  constructor() { }
+
+  dateValue = 'Click to select your date of birth';
+
+  formatDate(value: string) {
+    return format(parseISO(value), 'MM/dd/yyyy');
+  }
 
 }
