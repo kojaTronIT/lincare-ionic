87fca2d0e44a0394e03b4ee87b6573a29647f52f
Marko Cupic <marko.cupic@tron-it.com>
2022-02-17 09:01:25 +0100

    Add form validation and submit action

--- ionic-lincare-ordering-app/src/app/home/home.page.ts
+++ ionic-lincare-ordering-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IonDatetime } from '@ionic/angular';
 import { format, parseISO } from 'date-fns';
 
@@ -9,14 +10,45 @@ import { format, parseISO } from 'date-fns';
 })
 export class HomePage {
 
-  @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
-
-  dateValue = '';
-
-  constructor() { }
-
-  formatDate(value: string) {
-    return format(parseISO(value), 'MM/dd/yyyy');
+  public errorMessages = {
+    dateOfBirth: [
+      {type: 'required', message: 'Date of birth is a mandatory field'},
+      { type: 'pattern', message: 'Date should be in mm/dd/yyyy format (example: 05/19/1998)' }
+    ],
+    zipcode: [
+      { type: 'required', message: 'Zipcode is a mandatory field' },
+      { type: 'pattern', message: 'Zipcode should be 5 numbers long (example: 11000)' }
+    ]
   }
+
+  private dateOfBirthPattern = RegExp(/^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/g);
+  private zipcodePattern = RegExp(/^[0-9]{5}$/g);
+
+  // @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
+
+  // dateValue = '';
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  get dateOfBirth() {
+    return this.registrationForm.get('dateOfBirth');
+  }
+
+  get zipcode() {
+    return this.registrationForm.get('zipcode');
+  }
+
+  registrationForm = this.formBuilder.group({
+    dateOfBirth: [null, [Validators.required, Validators.pattern(this.dateOfBirthPattern)]],
+    zipcode: [null, [Validators.required, Validators.pattern(this.zipcodePattern)]]
+  });
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
+
+  // formatDate(value: string) {
+  //   return format(parseISO(value), 'MM/dd/yyyy');
+  // }
 
 }
