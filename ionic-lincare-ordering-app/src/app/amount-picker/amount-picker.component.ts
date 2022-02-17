3d56cdaed4f0931a8c702e094c49d5f8195b22a2
Marko Cupic <marko.cupic@tron-it.com>
2022-02-17 16:19:41 +0100

    Create amount picker component

---
+++ ionic-lincare-ordering-app/src/app/amount-picker/amount-picker.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amount-picker',
+  templateUrl: './amount-picker.component.html',
+  styleUrls: ['./amount-picker.component.scss'],
+})
+export class AmountPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
