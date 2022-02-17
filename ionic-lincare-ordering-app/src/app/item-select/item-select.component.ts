07c9ebbe9b5ca6ebfd9106a8bc6435e87b9260be
Marko Cupic <marko.cupic@tron-it.com>
2022-02-17 10:13:42 +0100

    Create item select component

---
+++ ionic-lincare-ordering-app/src/app/item-select/item-select.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-item-select',
+  templateUrl: './item-select.component.html',
+  styleUrls: ['./item-select.component.scss'],
+})
+export class ItemSelectComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {} 
+
+}
