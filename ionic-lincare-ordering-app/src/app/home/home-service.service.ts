97aab5437a01ce5edf9c41d975c478bd467b4c9c
Marko Cupic <marko.cupic@tron-it.com>
2022-02-24 13:30:46 +0100

    Update post methods

--- ionic-lincare-ordering-app/src/app/home/home-service.service.ts
+++ ionic-lincare-ordering-app/src/app/home/home-service.service.ts
@@ -10,12 +10,19 @@ export class HomeServiceService {
 
   constructor(private http: HttpClient) { }
 
-  public validateZip(zip: string): Observable<boolean> {
-    return this.http.post<boolean>("http://localhost:8080/api/v1/check_us_zip", {usZip: zip});
+  public validateZip(zip: string) {
+    return this.http.post("http://localhost:8080/api/v1/check_us_zip", {usZip: zip})
+      .pipe(
+        map((data: boolean) => {
+          return data;
+        }), catchError(error => {
+          return throwError(error);
+        })
+      );
   }
 
   public validateDobAndZip(zip: string, dob: string){
-    return this.http.post("http://localhost:8080/api/v1/validate_dob_zip", { dateOfBrith: dob, usZip: zip })
+    return this.http.post("http://localhost:8080/api/v1/validate_dob_zip", { dateOfBrith: dob, usZip: zip }, { responseType: 'text' })
     .pipe(
       map((data: string) => {
         return data;
