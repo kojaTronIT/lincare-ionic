7c2dcafee04dfce2dad751d37fc17688d3e31601
Marko Cupic <marko.cupic@tron-it.com>
2022-02-24 10:08:07 +0100

    Add service imports

--- ionic-lincare-ordering-app/src/app/app.module.ts
+++ ionic-lincare-ordering-app/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ItemSelectComponent } from './item-select/item-select.component';
 import { AmountPickerComponent } from './amount-picker/amount-picker.component';
+import { HomeServiceService } from './home/home-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ItemSelectComponent, AmountPickerComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ItemSelectComponent],
   bootstrap: [AppComponent],
 })
