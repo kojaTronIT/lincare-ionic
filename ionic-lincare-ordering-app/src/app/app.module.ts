136d7ab7960ebcacf92c551c13772c40f1ef6ba0
Marko Cupic <marko.cupic@tron-it.com>
2022-03-01 10:30:28 +0100

    Add dummy component to declarations

--- ionic-lincare-ordering-app/src/app/app.module.ts
+++ ionic-lincare-ordering-app/src/app/app.module.ts
@@ -9,11 +9,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ItemSelectComponent } from './item-select/item-select.component';
 import { AmountPickerComponent } from './amount-picker/amount-picker.component';
-import { HomeServiceService } from './home/home-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { DummyPageComponent } from './dummy-page/dummy-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemSelectComponent, AmountPickerComponent],
+  declarations: [AppComponent, ItemSelectComponent, AmountPickerComponent, DummyPageComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ItemSelectComponent],
