75d19a0ec06fa027ea04f1f69f9577a5d903c5d8
Marko Cupic <marko.cupic@tron-it.com>
2022-03-10 08:51:35 +0100

    Create address confirmation page

---
+++ src/app/address-confirmation/address-confirmation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-confirmation',
+  templateUrl: './address-confirmation.component.html',
+  styleUrls: ['./address-confirmation.component.scss'],
+})
+export class AddressConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
