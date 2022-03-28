4e6e62071cc772d668979ecce96149aa0a7f2518
Marko Cupic <marko.cupic@tron-it.com>
2022-03-28 15:59:34 +0200

    Minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -12,11 +13,13 @@ import { AmountPickerComponent } from './amount-picker/amount-picker.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DummyPageComponent } from './dummy-page/dummy-page.component';
 import { AddressConfirmationComponent } from './address-confirmation/address-confirmation.component';
+import { ErrorComponent } from './error/error.component';
+
 
 @NgModule({
-  declarations: [AppComponent, ItemSelectComponent, AmountPickerComponent, DummyPageComponent, AddressConfirmationComponent],
+  declarations: [AppComponent, ItemSelectComponent, AmountPickerComponent, DummyPageComponent, AddressConfirmationComponent, ErrorComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule, CommonModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ItemSelectComponent],
   bootstrap: [AppComponent],
 })
