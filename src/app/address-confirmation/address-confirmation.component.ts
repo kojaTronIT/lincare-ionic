c57216cf30fad7d053cfded4f2194fa9a3bb5f73
Marko Cupic <marko.cupic@tron-it.com>
2022-04-12 16:25:11 +0200

    Load address data from home page

--- src/app/address-confirmation/address-confirmation.component.ts
+++ src/app/address-confirmation/address-confirmation.component.ts
@@ -9,13 +9,25 @@ import { HomeServiceService } from '../home/home-service.service';
 })
 export class AddressConfirmationComponent implements OnInit {
 
-  constructor(private router: Router, private homeService: HomeServiceService) { }
+  patientName: any;
+  street: any;
+  apartment: any;
+  city: any;
+  state: any;
+  zipCode: any;
 
-  addressArray: any;
+  constructor(private router: Router, private homeService: HomeServiceService) {
+    this.patientName = this.router.getCurrentNavigation().extras.state.patientName;
+    this.street = this.router.getCurrentNavigation().extras.state.street;
+    this.apartment = this.router.getCurrentNavigation().extras.state.apartment;
+    this.city = this.router.getCurrentNavigation().extras.state.city;
+    this.state = this.router.getCurrentNavigation().extras.state.state;
+    this.zipCode = this.router.getCurrentNavigation().extras.state.zipCode;
+   }
 
   ngOnInit() {
-    this.addressArray = JSON.parse(localStorage.getItem("shipping_address"));
-    console.log(localStorage.getItem("shipping_address"));
+    // this.addressArray = JSON.parse(localStorage.getItem("shipping_address"));
+    // console.log(localStorage.getItem("shipping_address"));
   }
 
   onYes() {
@@ -24,7 +36,7 @@ export class AddressConfirmationComponent implements OnInit {
 
     this.homeService.logUserActions(
       localStorage.getItem("action"), localStorage.getItem("actionLocation"), localStorage.getItem("one_time_code")
-      ).subscribe({
+    ).subscribe({
       next: (data) => console.log(data),
       error: (error) => console.log(error.error)
     })
