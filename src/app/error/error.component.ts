f3d35c38558cdcc8e04f6c68584c6014a7780379
Marko Cupic <marko.cupic@tron-it.com>
2022-04-29 15:45:55 +0200

    Update error.component.ts

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -10,30 +10,44 @@ export class ErrorComponent implements OnInit {
 
   message;
 
-  flag = false;
+  flag: boolean;
 
   constructor(private homeService: HomeServiceService) { }
 
   ngOnInit() {
-    this.homeService.displayMessageForAction(localStorage.getItem("messageKey")).subscribe({
-      next: (data) => { 
-        this.message = data.message 
+    this.checkMessage();
 
-        if (data.message === "Your request has been cancelled and no further action is needed.") {
+    this.homeService.displayMessageForAction(localStorage.getItem("messageKey")).subscribe({
+      next: (data) => {
+
+        if(localStorage.getItem("messageKey") === "CANCEL") {
           this.flag = true;
         } else {
           this.flag = false;
         }
+
+        this.message = data.message;
       },
-      error: (error) => this.message = "We were not able to process your request. Please contact your local center (PH Placeholder)"
+      error: (error) => console.log(error.error) 
     });
 
     this.homeService.logUserActions(
-      localStorage.getItem("action"), localStorage.getItem("actionLocation"), localStorage.getItem("one_time_code")
+      localStorage.getItem("action"), 
+      localStorage.getItem("actionLocation"), 
+      localStorage.getItem("one_time_code")
     ).subscribe({
       next: (data) => console.log(data),
       error: (error) => console.log(error.error)
     })
+
+  }
+
+  checkMessage() {
+    if (this.message === undefined || this.message === null) {
+      this.message =
+        "We were not able to handle your request." +
+        "Please contact your local center (PH Placeholder)";
+    }
   }
 
 }
